1a5d038097de28f56ddb9b801f69bc22fb5c1a24
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-07-05 18:59:32 -0600

    progress made

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
+//function nameMenuItem(menuItemName) {console.log('Delicious ' + menuItemName)
+//};
+//nameMenuItem(chaiItem)
+//nameMenuItem(burritoItem)
 
+function nameMenuItem(menuItem){
+  return 'Delicious ' + menuItem
 }
+
+function createMenuItem(name, price, type){
+  console.log(name)
+  var foodItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+return foodItem
+};
+
+
+function addIngredients(){
+
+var ingredients = [];
+
+
+ingredients.push()
+
+return ingredients.push('cheese')
+};
+
+
+
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
